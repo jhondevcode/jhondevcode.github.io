f72a70fa54f1bf41789a07ccae5d7251b5a35c5f
jhondevcode <jhondev.code@gmail.com>
2022-06-19 20:39:31 -0500

    Main react component

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+import "./index.scss";
+import Navbar from "./Navbar";
+import Main from "./Main";
+import React from "react";
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Main />
+    </React.Fragment>
+  );
+};
+
+export default App;
