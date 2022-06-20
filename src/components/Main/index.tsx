d907c8cfc5b526a9fd039a28038e99b50677079c
jhondevcode <jhondev.code@gmail.com>
2022-06-19 20:46:13 -0500

    Main container

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,14 @@
+import Project from "../Project";
+import "./index.scss";
+
+const Main = () => {
+  return (
+    <main>
+      <div className="projects"></div>
+      <div className="social"></div>
+      <div className="testimonial"></div>
+    </main>
+  );
+}
+
+export default Main;
